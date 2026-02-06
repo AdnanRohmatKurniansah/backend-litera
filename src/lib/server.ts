665050ea7a3491f100ec50cf59f8f136d00d38a1
AdnanRohmatKurniansah <atnandimas@gmail.com>
2026-02-06 17:40:34 +0700

    fix: add cors origin

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -23,6 +23,8 @@ const createServer = () => {
     cors({
       origin: [
         'http://localhost:5173',
+        'http://localhost:5174',
+        'http://localhost:3000',
       ],
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
