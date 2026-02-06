31418f8f970d529120d18fde60762eff38de7b42
AdnanRohmatKurniansah <atnandimas@gmail.com>
2026-02-06 18:06:07 +0700

    add new cors origin

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -19,13 +19,23 @@ const createServer = () => {
 
   app.use(express.json())
 
+  const allowedOrigins = [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://localhost:3000',
+  ]
+
   app.use(
     cors({
-      origin: [
-        'http://localhost:5173',
-        'http://localhost:5174',
-        'http://localhost:3000',
-      ],
+      origin: (origin, callback) => {
+        if (!origin) return callback(null, true)
+
+        if (allowedOrigins.includes(origin)) {
+          return callback(null, true)
+        }
+
+        return callback(new Error('Not allowed by CORS'))
+      },
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       allowedHeaders: ['Content-Type', 'Authorization']
