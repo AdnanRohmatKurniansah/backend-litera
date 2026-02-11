b257cd6782883562bb5cdda7c890889741d779ba
AdnanRohmatKurniansah <atnandimas@gmail.com>
2026-02-11 17:46:24 +0700

    fix: article date validation

--- src/validations/article.validation.ts
+++ src/validations/article.validation.ts
@@ -3,11 +3,15 @@ import z from 'zod'
 export const ArticleCreateSchema = z.object({
   title: z.string().min(1, 'Title is required').max(200),
   content: z.string().min(1, 'Content is required'),
-  published_at: z.string().min(1, 'Published At is required')
+  published_at: z.string().min(1, 'Published At is required').transform((val) => {
+    return new Date(val).toISOString()
+  })
 })
 
 export const ArticleUpdateSchema = z.object({
   title: z.string().min(1, 'Title is required').max(200).optional(),
   content: z.string().min(1, 'Content is required').optional(),
-  published_at: z.string().min(1, 'Published At is required').optional()
+  published_at: z.string().min(1, 'Published At is required').transform((val) => {
+    return new Date(val).toISOString()
+  }).optional()
 })
